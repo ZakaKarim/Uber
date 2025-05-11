d301186202bb1242d0ebd185015e0e2f45b3b580
ZakaKarim <link3zakakarim@gmail.com>
2025-05-12 03:01:41 +0500

    Wroking on the Location and Vehicle Panel

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
+        <h4 className='font-medium'> Noor Street  Walton Road Lahore Cantt</h4>
+        </div>
+         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
+        <h4 className='font-medium'> kashif Traders  Walton Road Lahore Cantt</h4>
+        </div>
+         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
+        <h4 className='font-medium'> Model Town Link Road Lahore </h4>
+        </div>
+         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
+        <h4 className='font-medium'> Johar town block G</h4>
+        </div>
+         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
+        <h4 className='font-medium'> DHA Phase 5 DevSarch</h4>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
