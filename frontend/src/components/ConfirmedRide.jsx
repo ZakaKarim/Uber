1a742871d1ad6f080af8cbaa7a79139855d239bf
ZakaKarim <link3zakakarim@gmail.com>
2025-05-15 03:46:06 +0500

    Working on User UI

---
+++ frontend/src/components/ConfirmedRide.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const ConfirmedRide = () => {
+    return (
+        <div>
+            <h1 className='p-1 text-center top-0 w-[95%] absolute' onClick={() => {
+                props.setVehiclePanel(false)
+            }} >
+                <i class="text-3xl text-gray-400 ri-arrow-down-wide-fill"></i>
+            </h1>
+            <h2 className='font-medium text-2xl mb-5'>Confirm your Ride</h2>
+
+            <div className='flex gap-3 justify-between items-center flex-col'>
+                <img className='h-20' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png" alt="" srcset="" />
+                <div className='w-full'>
+                    <div className='flex items-center gap-5'>
+                        <i className="text-lg ri-map-pin-2-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562-11-A</h3>
+                            <p className='text-gray-600 text-base -mt-1'>Model Town Link Road Lahore Cantt </p>
+                        </div>
+                    </div>
+
+                     <div className='flex items-center gap-5 mt-2'>
+                        {/* <i className="text-lg ri-map-pin-2-fill"></i> */}
+                        <i className="text-lg ri-map-pin-user-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>E26/25-4</h3>
+                            <p className='text-gray-600 text-base -mt-1'>Islam Nagar Walton Road Lahore Cantt</p>
+                        </div>
+                    </div>
+                   
+                     <div className='flex items-center gap-5 mt-2'>
+                        {/* <i className="text-lg ri-map-pin-2-fill"></i> */}
+                        <i className="text-lg ri-currency-fill"></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>Pkr 350</h3>
+                            <p className='text-gray-600 text-base -mt-1'>Discount Rate</p>
+                        </div>
+                    </div>
+
+                </div>
+                <button className='w-full bg-green-500 text-white font-semibold p-2 rounded-lg'>
+                    Confirm Ride
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ConfirmedRide
--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+  console.log(props)
+  const locations = [
+    "Noor Street  Walton Road Lahore Cantt",
+    "kashif Traders  Walton Road Lahore Cantt",
+    "Model Town Link Road Lahore",
+    "Johar town block G",
+    "DHA Phase 5 DevSarch"
+  ]
   return (
     <div>
-        <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
+
+      {
+        locations.map(function(elem,idx){
+          return  <div key={idx} onClick={()=>{
+            props.setVehiclePanel(true)
+            props.setPanelOpen(false)
+          }} className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
         <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
-        <h4 className='font-medium'> Noor Street  Walton Road Lahore Cantt</h4>
+        <h4 className='font-medium'>{elem}</h4>
         </div>
-         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
-        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
-        <h4 className='font-medium'> kashif Traders  Walton Road Lahore Cantt</h4>
-        </div>
-         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
-        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
-        <h4 className='font-medium'> Model Town Link Road Lahore </h4>
-        </div>
-         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
-        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
-        <h4 className='font-medium'> Johar town block G</h4>
-        </div>
-         <div className='flex gap-4 border-1 rounded-xl border-gray-50 active:border-black p-2 m-2 items-center justify-start'>
-        <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full m-2'><i className="ri-map-pin-2-line"></i></h2>
-        <h4 className='font-medium'> DHA Phase 5 DevSarch</h4>
-        </div>
+        })
+      }
     </div>
   )
 }
