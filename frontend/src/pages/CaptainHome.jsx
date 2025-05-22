37abb5f294f7130cd3cfa6cbfc0527f149373bf8
Zaka_Karim <link3zakakarim@gmail.com>
2025-05-22 21:02:00 +0500

    Working on Captain UI

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import CaptainDetails from "../components/CaptainDetails";
 import RidePopUp from "../components/RidePopUp";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const CaptainHome = () => {
+
+  const [ridePopPanel, setRidePopPanel] = useState(true);
+  const RidePopPanelRef = useRef(null)
+
+   // GSAP TO OPEN THE RidePopPanel when clicking on icon
+  useGSAP(
+    function () {
+      if (ridePopPanel) {
+        gsap.to(RidePopPanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(RidePopPanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [ridePopPanel]
+  );
+
+
   return (
     <div className="h-screen">
       <div className="fixed p-4 top-0 flex items-center justify-between w-full">
@@ -31,8 +54,10 @@ const CaptainHome = () => {
       <div className="h-2/5 p-6">
         <CaptainDetails />
       </div>
-      <div className="fixed w-full z-10 bottom-0  bg-white px-3 py-10 pt-12">
-        <RidePopUp />
+      <div ref={RidePopPanelRef} className="fixed w-full z-10 bottom-0 translate-y-full  bg-white px-3 py-10 pt-12">
+        <RidePopUp
+        setRidePopPanel={setRidePopPanel}
+         />
       </div>
     </div>
   );
