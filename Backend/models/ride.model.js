65642beb2aceb9944115a62344727d95254cf5bb
Zaka_Karim <link3zakakarim@gmail.com>
2025-05-29 20:54:31 +0500

    Ride model is completed

---
+++ Backend/models/ride.model.js
@@ -0,0 +1,49 @@
+import e from "express";
+import { sign } from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const rideSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    captain:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Captain",
+    },
+    pickup: {
+        type: String,
+        required: true,
+    },
+    destination: {
+        type: String,
+        required: true,
+    },
+    fare:{
+        type: Number,
+        required: true,
+    },
+    status:{
+        type: String,
+        enum: ["pending", "accepted", "ongoing", "completed", "cancelled"],
+        default: "pending",
+    },
+    duration: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+    paymentID:{
+        type: String,
+    },
+    orderID:{
+        type: String,
+    },
+    signature:{
+        type: String,
+    },
+});
+
+export const Ride = mongoose.model("Ride", rideSchema);
---
+++ Backend/routes/ride.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+const { body } = require("express-validator");
+import { authUser } from "../middlewares/auth.middleware.js";
+
+//Route to 
+router.post('/create-ride',
+    body('userId').isString().isLength({min: 24,max: 24}).withMessage('User ID is required ..Invalid Id'),
+    body('pickup').isString().isLength({min: 3}).withMessage('Pickup location is required'),
+    body('destination').isString().isLength({min: 3}).withMessage('Destination location is required'),
+)
+
+export default router;
